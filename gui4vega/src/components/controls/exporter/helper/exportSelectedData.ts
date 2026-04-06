0e5d6ca66f0b21bd5d4bfa01e496a33df8bdaa23
Štěpán Faragula <farag844@students.zcu.cz>
2026-04-06 08:28:13 +0200

    Added typedoc

--- gui4vega/src/components/controls/exporter/helper/exportSelectedData.ts
+++ gui4vega/src/components/controls/exporter/helper/exportSelectedData.ts
@@ -1,9 +1,31 @@
+/**
+ * Exported Vega content with separated specification, datasets, and signals.
+ * Datasets and signals that are present in the attributes are not included in the specification.
+ */
 export interface ExportedData {
+    /**
+     * The Vega specification as a JSON string, with the selected datasets and signals removed.
+     */
     spec: string;
+    /**
+     * An array of JSON strings representing the selected datasets that were extracted from the specification.
+     * Each string contains the dataset name and its values.
+     */
     datasets: string[];
+    /**
+     * An array of JSON strings representing the selected signals that were extracted from the specification.
+     * Each string contains the signal name and its value.
+     */
     signals: string[];
 }
 
+/**
+ * Function to export selected datasets and signals from a Vega specification.
+ * @param specString - The Vega specification as a JSON string from which to extract the selected datasets and signals.
+ * @param datasetNames - An array of dataset names to be exported.
+ * @param signalNames - An array of signal names to be exported.
+ * @return An object containing the modified Vega specification with the selected datasets and signals extracted, and arrays of the exported datasets and signals as JSON strings.
+ */
 export function exportSelectedData(specString: string, datasetNames: string[] = [], signalNames: string[] = []): ExportedData {
     let specObj: Record<string, unknown>;
 
